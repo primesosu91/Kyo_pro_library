ffdf43d88c447a80a83b671d9902c142928ee739
primesosu91 <ryosuke.sosu.91@gmail.com>
2026-08-12 09:13:58 +0900

    Create Base.cpp

---
+++ Math/Base.cpp
@@ -0,0 +1,69 @@
+// 10進数とN進数文字列・配列の相互変換ライブラリ
+//
+// example:
+//
+// ABC 336 C - Even Digits
+// https://atcoder.jp/contests/abc336/submissions/me
+// 5進数変換
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+struct Base {
+    // 文字列(base進数)を10進数の整数に変換
+    static ll base_to_dec(const string& s, ll base) {
+        ll res = 0;
+        for (char c : s) {
+            res *= base;
+            if ('0' <= c && c <= '9') {
+                res += (c - '0');
+            } else if ('A' <= c && c <= 'Z') {
+                res += (c - 'A' + 10);
+            }
+        }
+        return res;
+    }
+
+    // 10進数の整数を文字列(base進数)に変換
+    static string dec_to_base(ll val, ll base) {
+        if (val == 0) return "0";
+        string res = "";
+        while (val > 0) {
+            ll rem = val % base;
+            if (rem < 10) res += (char)('0' + rem);
+            else res += (char)('A' + (rem - 10));
+            val /= base;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+
+    // from_base進数の文字列をto_base進数の文字列に直接変換
+    static string base_to_base(const string& s, ll from_base, ll to_base) {
+        return dec_to_base(base_to_dec(s, from_base), to_base);
+    }
+
+    // 10進数の整数をbase進数分解し、下位桁から順に格納した配列を返す
+    static vector<ll> dec_to_vec(ll val, ll base) {
+        if (val == 0) return {0};
+        vector<ll> res;
+        while (val > 0) {
+            res.push_back(val % base);
+            val /= base;
+        }
+        return res;
+    }
+
+    // 下位桁から順に格納された配列(base進数)を10進数の整数に復元
+    static ll vec_to_dec(const vector<ll>& digits, ll base) {
+        ll res = 0;
+        ll mul = 1;
+        for (ll d : digits) {
+            res += d * mul;
+            mul *= base;
+        }
+        return res;
+    }
+};
